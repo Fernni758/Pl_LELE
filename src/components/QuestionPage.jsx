d253f6f4689ddcf1076eff1614314cefe1e29024
unknown <galicia.fer.03@gmail.com>
2026-05-12 10:34:07 -0600

    Descripción de los cambios

--- src/components/QuestionPage.jsx
+++ src/components/QuestionPage.jsx
@@ -1,9 +1,9 @@
-import { useState,useEffect, use } from "react";
-import "../styles/questionPage.css";    
+import { useState, useEffect, use } from "react";
+import "../styles/questionPage.css";
 
 
-function QuestionPage({onYesClick}) {
-    const mensajes =[
+function QuestionPage({ onYesClick }) {
+    const mensajes = [
         "seguraaaa que no???",
         "estas segurisimaa???",
         "Andaaaaaaaa",
@@ -12,52 +12,41 @@ function QuestionPage({onYesClick}) {
         "HDLV ya di que si ",
         "Yo creo que es pq me odias",
         "aceptalo :(",
-        "andale di que si <3" ,
+        "andale di que si <3",
     ];
 
     //Estado para cambiar mensjae del texto de no a la lista de mensajes
-    const [noMessage, setNoMessage]=useState("no");
+    const [noMessage, setNoMessage] = useState("no");
 
     //Estado para manejar el indice de los mensajes 
-    const [messageIndex, setMessageIndex]= useState(0);
+    const [messageIndex, setMessageIndex] = useState(0);
 
     //Estado para cambiar el tamñao de la leta del boton si
-    const [yesBtnSize, setYesBtnSize]= useState(2);
+    const [yesBtnSize, setYesBtnSize] = useState(2);
 
-    useEffect (()=> {
+    useEffect(() => {
         console.log("Tamaño de la letra del boton si es de: ", yesBtnSize);
 
-    },[yesBtnSize]);
+    }, [yesBtnSize]);
 
     //Funcnion para manejar el click en el boton de no 
     const handleNoClick = () => {
-        setNoMessage (mensajes[messageIndex])
-        setMessageIndex (messageIndex + 1);
-        setYesBtnSize ((tamañoActual) => tamañoActual *1.5);
+        setNoMessage(mensajes[messageIndex])
+        setMessageIndex(messageIndex + 1);
+        setYesBtnSize((tamañoActual) => tamañoActual * 1.5);
 
     }
 
     return (
 
         <div className="question-container">
-            <h1 className="question-title">¿Mañana vamos por un mathca amor?</h1>
+            <h1 className="question-title">felicidades vas mejorando muuuy rapido    TQM</h1>
             <div className="buttons-container">
-                <button 
-                    className= "yes-button"
-                    onClick={onYesClick}
-                    style = {{fontSize: `${yesBtnSize}rem`}}>
-                    Si
-                </button>
-                <button 
-                    className="no-button"
-                    onClick={handleNoClick}>
-                    {noMessage}
-                </button>
             </div>
             <div className="image-container">
                 <img
-                    src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3N6Y2xsN3RuZ28xZHRnNzdjdjZnOXN1ZDBqdTZueXV1OXBpNnR4diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/4umTDLyx8TrNxytzqg/giphy.gif" 
-                            alt="Gatito enamorado"
+                    src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExaDczaWo2NTY2bjhiNjYwbHQybno3a2o2NWM3MGFmbThtYWE1dXd5NCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/TeymOuTOcUtyXFGSKf/giphy.gif"
+                    alt="Gatito enamorado"
                 />
             </div>
         </div>
